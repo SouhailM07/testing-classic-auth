79bc2c1bfb49b6b401d2333d1f9e0ff98d8b7b59
souhail <lightshadow416@gmail.com>
2024-02-09 20:56:28 +0100

    adding forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   let links: string[] = ["login", "signIn"];
   return (
-    <main className="flex min-h-screen items-center justify-center ">
+    <main className="flex min-h-screen items-center justify-center">
       <article className=" space-y-[3rem]">
         <h1 className="text-[2rem] font-bold">Welcome to the shadows</h1>
         <section className="text-center space-x-[4rem] text-white">
@@ -12,7 +12,7 @@ export default function Home() {
               <Link
                 href={e}
                 key={i}
-                className="bg-slate-900 p-3 inline-block rounded-md hover:bg-slate-700 duration-300 transition-all"
+                className="bg-slate-900 p-3 inline-block rounded-sm hover:bg-slate-700 duration-300 transition-all"
               >
                 {e}
               </Link>
---
+++ src/app/signIn/page.tsx
@@ -0,0 +1,118 @@
+"use client";
+// types
+import { inputs } from "@/types";
+// forms
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+// shadcn
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    message: "name must be at least 3 characters",
+  }),
+  email: z.string().email({
+    message: "Invalid email",
+  }),
+  password: z.string().min(8, {
+    message: "password must be at least 8 characters",
+  }),
+});
+
+/*=====================================================================================*/
+// main component section
+/*=====================================================================================*/
+export default function SignIn_page() {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-[1.7rem] font-bold underline">Sign In</h1>
+        <SignIn />
+      </main>
+    </>
+  );
+}
+
+/*=====================================================================================*/
+// small components
+/*=====================================================================================*/
+
+const SignIn = () => {
+  // Define form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    //! set your default values here
+    defaultValues: {
+      name: "",
+    },
+  });
+  //  Defining a submit handler
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // start
+    console.log(values);
+  };
+  // Define inputs
+  let inputs: inputs[] = [
+    {
+      name: "name",
+      label: "Name",
+      placeholder: "enter your name",
+      inputType: "text",
+    },
+    {
+      name: "email",
+      label: "Email",
+      placeholder: "example@gmail.com",
+      inputType: "text",
+    },
+    {
+      name: "password",
+      label: "Password",
+      placeholder: "your password",
+      inputType: "password",
+    },
+  ];
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+          {inputs.map((e: any, i) => {
+            return (
+              <FormField
+                key={i}
+                control={form.control}
+                name={e.name}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{e.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        type={e.inputType}
+                        placeholder={e.placeholder}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            );
+          })}
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
