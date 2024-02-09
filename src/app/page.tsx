8b7149af7063ed5c30f36a5ace1d7129973ebd56
souhail <lightshadow416@gmail.com>
2024-02-09 20:26:04 +0100

    creating the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 //
+import Link from "next/link";
 export default function Home() {
+  let links: string[] = ["login", "signIn"];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      sb
+    <main className="flex min-h-screen items-center justify-center ">
+      <article className=" space-y-[3rem]">
+        <h1 className="text-[2rem] font-bold">Welcome to the shadows</h1>
+        <section className="text-center space-x-[4rem] text-white">
+          {links.map((e, i) => {
+            return (
+              <Link
+                href={e}
+                key={i}
+                className="bg-slate-900 p-3 inline-block rounded-md hover:bg-slate-700 duration-300 transition-all"
+              >
+                {e}
+              </Link>
+            );
+          })}
+        </section>
+      </article>
     </main>
   );
 }
